f60d22044ceddadd14a53f83d4291e8821589d43
ASUS <ASUS@LAPTOP-2JE99TTR>
2020-09-18 16:51:34 +0700

    Add project files.

---
+++ Project03on18092020/Source.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+	int i, j;
+	for (i = 0; i <= 5; i++) {
+		for (j = 0; j <= 6; j++) {
+			if ((i == 0 && j % 3 != 0) || ((i == 1) && (j % 3 == 0)) || (i - j == 2) || (i + j == 8))
+				printf("*");
+			else printf(" ");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
